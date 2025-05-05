0ca10e04eba69556ccc15bf19abeef0572108903
Shaikh Ayesha <ayeshayy444@gmail.com>
2025-05-05 16:15:59 +0500

    model updated

--- lib/model/quote_model.dart
+++ lib/model/quote_model.dart
@@ -5,11 +5,10 @@ class Quote {
 
   Quote({required this.quote, required this.author, required this.category});
 
-  factory Quote.fromJson(Map<String, dynamic> json) {
-    return Quote(
-      quote: json['quote'],
-      author: json['author'],
-      category: json['category'],
-    );
-  }
+  Quote.fromJson(Map<String, dynamic> json)
+      :
+        quote = json['quote'],
+        author = json['author'],
+        category = json['category'];
+        
 }
